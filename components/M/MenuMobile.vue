3be314fddc1bce1d467ba058ed8b0c314afc47bd
lucas <piresnogueiralucas@gmail.com>
2024-04-02 15:15:08 -0300

    Update CSS and mobile menu behavior

--- components/M/MenuMobile.vue
+++ components/M/MenuMobile.vue
@@ -15,6 +15,7 @@ const sizeMenu = ref(50);
 
 const handleClick = () => {
   menuIsOpen.value = !menuIsOpen.value;
+  document.body.style.overflow = menuIsOpen.value ? "hidden" : "auto";
 };
 
 const handleTouch = (event: TouchEvent) => {
@@ -24,9 +25,10 @@ const handleTouch = (event: TouchEvent) => {
   }
 };
 
-const handleTouchEnd = () => {
+const handleTouchEnd = (event: TouchEvent) => {
   if (sizeMenu.value >= MAX_SIZE || sizeMenu.value <= MIN_SIZE) {
     menuIsOpen.value = false;
+    document.body.style.overflow = "auto";
   }
   sizeMenu.value = 50;
 };
@@ -47,38 +49,37 @@ const navigateTo = (path: string) => {
     v-bind:icon="menuIsOpen ? 'i-carbon-close' : 'i-carbon-menu'"
     v-on:click="handleClick"
   />
-  <div>
-    <span
-      class="fixed inset-0 z-10 bg-neutral-900/60 block"
+
+  <span
+    class="fixed inset-0 z-10 bg-neutral-900/60 block"
+    v-on:click="handleClick"
+    v-show="menuIsOpen"
+  />
+
+  <nav
+    class="fixed inset-x-0 bottom-0 top-0 z-20 bg-neutral-950 transition-transform duration-300 rounded-t-xl"
+    v-bind:style="{
+      transform: menuIsOpen ? `translateY(${sizeMenu}%)` : 'translateY(100%)',
+    }"
+  >
+    <button
+      class="h-2 w-20 m-(x-auto t-4) block rounded bg-neutral-700"
+      v-on:touchstart="buttonDown = true"
+      v-on:touchend="handleTouchEnd"
       v-on:click="handleClick"
-      v-show="menuIsOpen"
+      v-on:touchmove="handleTouch"
     />
 
-    <nav
-      class="fixed inset-x-0 bottom-0 top-0 z-20 bg-neutral-950 transition-transform duration-300 rounded-t-xl"
-      v-bind:style="{
-        transform: menuIsOpen ? `translateY(${sizeMenu}%)` : 'translateY(100%)',
-      }"
-    >
-      <button
-        class="h-2 w-20 m-(x-auto t-4) block rounded bg-neutral-700"
-        v-on:touchstart="buttonDown = true"
-        v-on:touchend="handleTouchEnd"
-        v-on:click="handleClick"
-        v-on:touchmove="handleTouch"
+    <menu class="space-y-2 p-4">
+      <AButton
+        custom="w-full text-sm"
+        position="left"
+        v-bind:key="page.name"
+        v-bind:icon="page.icon"
+        v-bind:text="page.name"
+        v-on:click="navigateTo(page.path)"
+        v-for="page in pages"
       />
-
-      <menu class="space-y-2 p-4">
-        <AButton
-          custom="w-full text-sm"
-          position="left"
-          v-bind:key="page.name"
-          v-bind:icon="page.icon"
-          v-bind:text="page.name"
-          v-on:click="navigateTo(page.path)"
-          v-for="page in pages"
-        />
-      </menu>
-    </nav>
-  </div>
+    </menu>
+  </nav>
 </template>
